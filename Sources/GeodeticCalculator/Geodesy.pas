2efd4638e6febf72c4bdda0cb628c1d5ce736787
gilleslabelle <gilleslabelle@live.ca>
2017-09-25 09:44:59 -0400

    Update Geodesy.pas
    [D9-1] terminé les corrections du header bis

--- Sources/GeodeticCalculator/Geodesy.pas
+++ Sources/GeodeticCalculator/Geodesy.pas
@@ -1,11 +1,8 @@
 unit Geodesy;
 // Gavaghan.Geodesy by Mike Gavaghan
-//
 // http://www.gavaghan.org/blog/free-source-code/geodesy-library-vincentys-formula/
-//
 // This code may be freely used and modified on any personal or professional
 // project.  It comes with no warranty.
-//
 // BitCoin tips graciously accepted at 1FB63FYQMy7hpC2ANVhZ5mSgAZEtY1aVLf
 //
 
